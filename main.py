7e08e5edd72fa4dcdbfa57dc6b69c055cb3ed677
SquateDev <sosantyan198@gmail.com>
2024-07-27 12:16:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,10 +17,15 @@ time.sleep(2);
 os.system("clear");
 print(Fore.RED + text);
 print("\n\n\n");
-print(Fore.GREEN+"[ 1 ]"+Fore.WHITE+" Phone Searc");
+print(Fore.RED+"[ 1 ]"+Fore.WHITE+" Phone Searc");
 print(Fore.RED+"[ 2 ]"+Fore.WHITE+" IP Searc");
 user_input = input("Введите Число : ");
 if "1" in user_input.lower():
- print("Вы выбрали Поиск По номеру !");
+ os.system("clear");
+ print(Fore.RED + text);
+ print(Fore.RED+"Вы выбрали Поиск По номеру !");
+ phone_num = input(Fore.RED + "Введите Номер +7 : ");
+if "2" in user_input.lower():
+  print(Fore.RED +"Вы выбрали поиск По ИП");
 
 
