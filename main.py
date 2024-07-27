12d3f91edf87433423b84ea3c25f5518e17dfcdf
SquateDev <sosantyan198@gmail.com>
2024-07-27 12:09:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time 
 import os
 from colorama import Fore, Style
+import subprocess 
 
 text = """██████╗░███████╗░█████╗░███╗░░██╗░█████╗░███╗░░██╗
 ██╔══██╗██╔════╝██╔══██╗████╗░██║██╔══██╗████╗░██║
@@ -13,5 +14,13 @@ print("Loading... |");
 time.sleep(2);
 print("Loading... /");
 time.sleep(2);
-os.system("cls");
-print(Fore.GREEN + text);
+os.system("clear");
+print(Fore.RED + text);
+print("\n\n\n");
+print(Fore.GREEN+"[ 1 ]"+Fore.WHITE+" Phone Searc");
+print(Fore.RED+"[ 2 ]"+Fore.WHITE+" IP Searc");
+user_input = input("Введите Число : ");
+if "1" in user_input.lower():
+ print("Вы выбрали Поиск По номеру !");
+
+
